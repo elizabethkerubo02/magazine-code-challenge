f38f47f2b3e8a1798aeb58fc32d85daa28dd190e
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-05-29 22:02:15 +0300

    added comments

--- magazine/article.rb
+++ magazine/article.rb
@@ -1,24 +1,27 @@
 class Article
     attr_reader :magazine, :title
-    @@all=[] # Class variable to store all instances of Article
-
-
-    def initialize(author,magazine,title)
-        @author = author
-        @magazine = magazine
-        @title = title
-        @@all << self
+    @@all = [] # Class variable to store all instances of Article
+  
+    def initialize(author, magazine, title)
+      # Initialize method is called when a new instance of Article is created
+      # It takes author, magazine, and title as arguments and assigns them to instance variables
+      # author - the author associated with the article
+      # magazine - the magazine the article belongs to
+      # title - the title of the article
+      # @@all - the class variable that stores all instances of Article
+      # self - refers to the current instance of the Article class
+      # Adds the current instance to the @@all array
+      @@all << self
     end
-
-
+  
     # Returns the name of the author associated with the article
     def author
-        @author.name
+      author.name
     end
-
-      # Returns the @@all array, which contains all instances of the Article class
+  
+    # Returns the @@all array, which contains all instances of the Article class
     def self.all
-        @@all
+      @@all
     end
-
-end
+  end
+  
