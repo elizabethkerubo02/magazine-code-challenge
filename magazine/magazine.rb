ce75184bbc31314a038c21bb9885f23290543282
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-05-29 21:38:11 +0300

    created initialize method for name,categoty and class variable called all

--- magazine/magazine.rb
+++ magazine/magazine.rb
@@ -1,4 +1,12 @@
 class Magazine
     attr_accessor :name, :category
+    @@ll = []
+
+    def initialize(name,category)
+        @name = name
+        @category = category
+
+        @@ll << self
+    end
 
 end
